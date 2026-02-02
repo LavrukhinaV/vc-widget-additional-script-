821402297c12cd9539f6bab4bdaa6cc8243fc46d
Viktoriia Lavrukhina <tomenko_viktoriya@mail.ru>
2026-02-02 15:41:38 +0300

    Update script1.js

--- script1.js
+++ script1.js
@@ -99,7 +99,7 @@
   };
 
   fjs.parentNode.insertBefore(js, fjs);
-})(window, document, 'script', 'XoZ5', 'https://vc-widget-script.vercel.app/widget.js');
+})(window, document, 'script', 'XoZ5', 'https://cabinet.vivacrm.ru/widget.js');
 
 // Конфигурация и инициализация СНАРУЖИ onload (через очередь команд)
 function onDomReady(cb) {
